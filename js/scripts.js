08bdf5daafdc26acc6cf547aea729d73a3a164ed
beth foster and timmi mcguire <student@epicodus.com>
2016-01-19 15:42:43 -0800

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 var triangle = function(side1, side2, side3) {
 
   if ((side1 + side2 <= side3) || (side2 + side3 <= side1) || (side1 + side3 <= side2)){
-      return "not a triangle";
-  } else if (side1 === side2 === side3) {
+      return ("not a triangle");
+  } else if (side1 === side2 && side2 === side3 && side1 === side3) {
       return "equilateral";
   } else if ((side1 === side2) || (side2 === side3) || (side3 === side1)){
       return "isosceles";
@@ -17,21 +17,13 @@ $(document).ready(function() {
     var side3 = parseInt($("input#side3").val());
     var result = triangle(side1, side2, side3);
 
-side1 = parseInt($("input#side1").val(""));
-side2 = parseInt($("input#side2").val(""));
-side3 = parseInt($("input#side3").val(""));
-
-$(".year").text(year);
-
-if(!result) {
- $ ("#side1").text("equilateral");
-} else {
- $("#side2").text("");
-  }
- $("#result").show();
+// side1 = parseInt($("input#side1").val(""));
+// side2 = parseInt($("input#side2").val(""));
+// side3 = parseInt($("input#side3").val(""));
+//
 
 
-$("#result").show(result);
+$("#result").append(result);
    event.preventDefault();
  });
 });
